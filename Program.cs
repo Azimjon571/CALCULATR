49c348272af7cb99bb3eeab3a89e3a0bc1cdaf73
Azimjon <azimjon.007@mail.ru>
2022-10-05 13:17:04 +0200

    Switch operation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
 ﻿System.Console.Write("Type 1st number: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter operation(+, -, *, /, %");
+string operation = Console.ReadLine();
 System.Console.Write("Type 2nd number: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//Ternar operator
+
+string message = 
+    firstNumber >= 0 
+        ? "First numbeer is positive" 
+        : "First number is negative";
+
+System.Console.WriteLine(message);
 
 if(firstNumber>secondNumber)
 {
@@ -11,7 +22,29 @@ else if(firstNumber<secondNumber)
 {
     System.Console.WriteLine("2nd number greater than 1st number");
 }
-else if (firstNumber==secondNumber)
+else
 {
     System.Console.WriteLine("1st number equal to the 2nd number");
+}
+
+switch(operation)
+{
+    case "+":
+        System.Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+        break;
+    case "-":
+        System.Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+        break;
+    case "*":
+        System.Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+        break;
+    case "/":
+        System.Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+        break;
+    case "%":
+        System.Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
+        break;
+    default:
+        System.Console.WriteLine("Operation not Found!");
+        break;
 }
